d86cbec5ec3e29895ef652c349a277dea805f85a
hardeep singh <hardeepde27@gmail.com>
2024-06-11 13:07:55 +0530

    fix chat design

--- app/chat/_layout.tsx
+++ app/chat/_layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, TextInput } from "react-native";
 import ButtonComponent from "@/components/ButtonComponent";
 import InputComponent from "@/components/InputComponent";
 import CardComponent from "@/components/CardComponent";
@@ -9,6 +9,7 @@ import Header from "../header/_layout";
 import { GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
 import SvgUserIcon from '@/assets/images/userIcon.svg';
 import SvgWarningIcon from '@/assets/images/warning-s.svg';
+import SvgAttachmentIcon from '@/assets/images/attachment.svg';
 
 
 export default function Chat() {
@@ -16,68 +17,74 @@ export default function Chat() {
         <GestureHandlerRootView style={{ flex: 1 }}>
             <GradientBackgroundComponent>
                 <Header />
-                <CardComponent>
-                <ScrollView >
 
-                    <View style={{ flexDirection: "column", height: "85%", }}>
+                <View style={{ flexDirection: "column", height: "85%", backgroundColor: "#F7F7F7", marginHorizontal: 20, borderTopEndRadius: 20, borderTopLeftRadius: 20, padding: 20 }}>
+                    <ScrollView >
                         <View>
-                            <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-                                <View style={{ flexDirection: "row" }}>
-                                    <View style={{ position: "relative", marginRight: 6 }}>
-                                        <SvgUserIcon />
-                                        <View style={styles.online}></View>
+                            <View>
+                                <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+                                    <View style={{ flexDirection: "row" }}>
+                                        <View style={{ position: "relative", marginRight: 6 }}>
+                                            <SvgUserIcon />
+                                            <View style={styles.online}></View>
+                                        </View>
+                                        <View>
+                                            <Text style={{ fontSize: 16, color: "#0D172A" }}>Sienna</Text>
+                                            <Text style={{ fontSize: 10, color: "#3EA95D" }}>Available</Text>
+                                        </View>
                                     </View>
                                     <View>
-                                        <Text style={{ fontSize: 16, color: "#0D172A" }}>Sienna</Text>
-                                        <Text style={{ fontSize: 10, color: "#3EA95D" }}>Available</Text>
+                                        <SvgWarningIcon />
                                     </View>
                                 </View>
-                                <View>
-                                    <SvgWarningIcon />
+
+
+                            </View>
+
+                            <View style={{ position: "relative" }}>
+                                <View style={styles.leftContainer}>
+                                    <Text style={styles.leftMessage}>Good morning, Anna, how can I help you?</Text>
+
                                 </View>
-                            </View>
+                                <View style={[styles.leftContainer]}>
+                                    <View style={[styles.leftMessage]}>
+                                        <Text>
+                                            Select option:
+                                        </Text>
+                                        <Text style={styles.options}>1 - Book maintenance</Text>
+                                        <Text style={styles.options}>2 - I got a problem…</Text>
+
+                                        <Text style={styles.options}>3 - Contact Billion Bricks</Text>
+
+                                        <Text style={styles.options}>4 - Write a feedback</Text>
+                                        <Text style={styles.options}>5 - Other issue</Text>
 
 
-                        </View>
-                        <View style={{ position: "relative" }}>
-                            <View style={styles.leftContainer}>
-                                <Text style={styles.leftMessage}>Good morning, Anna, how can I help you?</Text>
+                                    </View>
 
-                            </View>
-                            <View style={[styles.leftContainer]}>
-                                <View style={[styles.leftMessage]}>
-                                    <Text>
-                                        Select option:
+                                </View>
+
+                                <View style={{ flexDirection: "row", justifyContent: "flex-end" }}>
+                                    <Text style={styles.rightMessage}>1 - Book maintenance</Text>
+                                </View>
+                                <View style={styles.leftContainer}>
+                                    <Text style={styles.leftMessage}>
+                                        Great! it is important to keep things tidy.
                                     </Text>
-                                    <Text style={styles.options}>1 - Book maintenance</Text>
-                                    <Text style={styles.options}>2 - I got a problem…</Text>
-
-                                    <Text style={styles.options}>3 - Contact Billion Bricks</Text>
-
-                                    <Text style={styles.options}>4 - Write a feedback</Text>
-                                    <Text style={styles.options}>5 - Other issue</Text>
-
 
                                 </View>
-
                             </View>
-
-                            <View style={{ flexDirection: "row", justifyContent: "flex-end" }}>
-                                <Text style={styles.rightMessage}>1 - Book maintenance</Text>
-                            </View>
-
 
                         </View>
-                    </View>
 
-                    <View>
-
-                        <ButtonComponent title="Log in" onPress={() => { router.push('dashboard') }} />
-
-
-                    </View>
                     </ScrollView>
-                </CardComponent>
+                    <View style={{ backgroundColor: "#E2E2DF", paddingHorizontal: 10, borderRadius: 37, flexDirection: "row", alignItems: "center" }}>
+                        <TouchableOpacity>
+                            <SvgAttachmentIcon />
+                        </TouchableOpacity>
+                        <TextInput style={{ width: "95%", padding: 10 }} />
+                    </View>
+                </View>
             </GradientBackgroundComponent>
         </GestureHandlerRootView>
 
@@ -90,11 +97,13 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 8,
         fontSize: 14,
-        marginTop:10
+        marginTop: 10
     },
     leftContainer: {
-        flexDirection: "row", 
-        justifyContent: "flex-start"
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        marginBottom: 10
+
     },
     leftMessage: {
         width: "100%",
